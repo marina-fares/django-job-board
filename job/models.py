a19114ea2dd27c3b29038d7a993be20c7c006208
marina-fares <marina.fares11@gmail.com>
2020-08-27 00:27:00 +0200

    add job views &uls

--- job/models.py
+++ job/models.py
@@ -20,7 +20,15 @@ class Job(models.Model):
     published_at = models.DateTimeField(auto_now=True)
     vacancy = models.IntegerField(default=0)
     experience = models.IntegerField(default=1)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
-     
+
+# one to many relationship
+class Category(models.Model):
+    name = models.CharField(max_length=25)
+
+    def __str__(self):
+        return self.name
+    
